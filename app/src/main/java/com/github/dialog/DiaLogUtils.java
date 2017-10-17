7a61b1f355c6341fd0d08e45920d2b33502be6e4
zhanggang <zhang834826337@qq.com>
2017-10-17 19:05:00 +0800

    依赖

---
+++ app/src/main/java/com/github/dialog/DiaLogUtils.java
@@ -0,0 +1,30 @@
+package com.github.dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+/**
+ * Created by zhanggang on 2017/10/17.
+ */
+
+public class DiaLogUtils {
+    public static void showMyDiaLog(final Context context, String title, String message) {
+        new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(context, "点击了确定！", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(context, "点击了取消！", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+    }
+}
